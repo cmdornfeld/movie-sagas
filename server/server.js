1a4d4edbeb9f6b2861002d9fb515d2143a93cbbd
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-20 15:31:55 -0600

    added moviesRouter with GET and added route to server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+const moviesRouter = require('./routes/movies.router')
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
+app.use('/movies', moviesRouter);
 
 
 /** ---------- START SERVER ---------- **/
