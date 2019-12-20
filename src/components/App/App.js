947ccb3755ee50efbd5c13926c4ded301cea24db
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-20 15:50:55 -0600

    imported Router and components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from '../Home/Home';
+import Details from '../Details/Details';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Router>
+          <h1 className="header">Movie Search</h1>
+          <Route exact path="/" component={ Home }/>
+          <Route path="/details" component={ Details }/>
+        </Router>
       </div>
     );
   }
