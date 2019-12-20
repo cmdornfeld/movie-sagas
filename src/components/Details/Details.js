f44fa5b0f175fe98f5d2455a5f6243b6927322ef
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-20 15:49:57 -0600

    added Details component skeleton

---
+++ src/components/Details/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+    render() {
+      return (
+        <>
+            <p>Details Component</p>
+        </>
+      );
+    }
+  }
+  
+  export default Details;
