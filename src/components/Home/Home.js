b9abeab3a04cc1ce8ba713c3efd381127cb60100
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-20 15:49:39 -0600

    added Home component skeleton

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+      return (
+        <>
+            <p>Home Component</p>
+        </>
+      );
+    }
+  }
+  
+  export default Home;
