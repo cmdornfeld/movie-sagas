f2973592dd2b6033b1e3f8e06195c3f93a9fe55e
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-21 16:15:40 -0600

    added 2nd GET route for details GET request with id

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -4,15 +4,27 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const queryText = `SELECT * FROM movies`;
-      pool.query(queryText)
-          .then( (result) => {
-              res.send(result.rows);
-          })
-          .catch( (error) => {
-            console.log(`Error on query ${error}`);
-          res.sendStatus(500);
-      });
-  });
+  const queryText = `SELECT * FROM movies`;
+    pool.query(queryText)
+        .then( (result) => {
+            res.send(result.rows);
+        })
+        .catch( (error) => {
+          console.log(`Error on query ${error}`);
+        res.sendStatus(500);
+    });
+});
 
-  module.exports = router;
+router.get('/:id', (req, res) => {
+  const queryText = `SELECT "title", "description" FROM movies WHERE id=$1;`;
+    pool.query(queryText, [req.params.id])
+        .then( (result) => {
+            res.send(result.rows);
+        })
+        .catch( (error) => {
+          console.log(`Error on query ${error}`);
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
