83d1c8d720fbf00223ec43fc3510fa23d9c3407f
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-21 16:21:49 -0600

    added Link and function to move to details component onClick

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
 
   componentDidMount() {
     this.props.dispatch({type: 'GET_MOVIES'});
+  }
+
+  goToDetails = (event, id) => {
+    this.props.dispatch({type: 'GET_DETAILS', payload: {id: id}})
   }
 
   render() {
@@ -16,7 +21,9 @@ class Home extends Component {
               <div key={i}>
                 <h2>{movie.title}</h2>
                 <br/>
-                <img src={movie.poster} />
+                <Link to='/details'>
+                  <img onClick={(event) => this.goToDetails(event, movie.id)} src={movie.poster} /> 
+                </Link>
                 <p>{movie.description}</p>
               </div>
             )
