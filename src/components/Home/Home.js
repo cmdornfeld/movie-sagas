97357101a9f3e2d3dabe33f9f42c8466eac87555
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-21 12:41:59 -0600

    imported reduxState to Home, and ability to map through results

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class Home extends Component {
-    render() {
-      return (
-        <>
-            <p>Home Component</p>
-        </>
-      );
-    }
+
+  componentDidMount() {
+    this.props.dispatch({type: 'GET_MOVIES'});
   }
+
+  render() {
+    return (
+      <>
+          <h1>Movies</h1>
+          {this.props.reduxState.map((movie, i) => {
+            return (
+              <div key={i}>
+                <h2>{movie.title}</h2>
+                <br/>
+                <img src={movie.poster} />
+                <p>{movie.description}</p>
+              </div>
+            )
+          })}
+      </>
+    );
+  }
+}
+
+const putReduxStateOnProps = (reduxState)=>({
+  reduxState: reduxState.moviesReducer
+});
   
-  export default Home;
+export default connect(putReduxStateOnProps)(Home);
