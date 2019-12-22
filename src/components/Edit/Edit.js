6eb3e278e82f4af7ae2af4a06afd7115d9ac013c
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-22 13:10:54 -0600

    updated Edit.js to include index of [0]

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -6,7 +6,7 @@ class Edit extends Component {
     state = {
         title: '',
         description: '',
-        id: this.props.reduxState.detailsReducer.id
+        id: this.props.details[0].id
     }
 
     backToDetails = () => {
@@ -14,14 +14,18 @@ class Edit extends Component {
     }
 
     handleChange = (event, propertyName) => {
+        console.log(event.target.value);
+        
         this.setState({
             [propertyName]: event.target.value
         })
     }
 
-    // editMovieInfo = (event, id) => {
-    //     this.props.dispatch({type: 'EDIT_MOVIE'})
-    // }
+    editMovieInfo = (event, id) => {
+        console.log(this.state);
+        this.props.dispatch({type: 'EDIT_MOVIE', payload: this.state})
+        this.props.history.push('/details');
+    }
 
     render(){
         return (
@@ -30,15 +34,15 @@ class Edit extends Component {
                 <input onChange={(event) => this.handleChange(event, 'title')} type="text" placeholder="title" />
                 <input onChange={(event) => this.handleChange(event, 'description')} type="text" placeholder="description" />
                 <button onClick={this.backToDetails}>Cancel</button>
-                {/* <button onClick={this.editMovie(this.props.reduxState.moviesReducer.id)}>Save</button> */}
-                {JSON.stringify(this.props.reduxState.detailsReducer.id)}
+                <button onClick={this.editMovieInfo}>Save</button>
+                {JSON.stringify(this.props.details[0].id)}
             </>
         )
     }
 }
 
 const putReduxStateOnProps = (reduxState)=>({
-    reduxState
+    details: reduxState.detailsReducer
 });
 
 export default connect(putReduxStateOnProps)(Edit)
